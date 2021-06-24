6b9f8eb80de7b95fe74d469f47b61a78708e36d8
Stephan <stephangdenis@gmail.com>
2021-06-24 16:49:46 -0300

    [Login]: firebase + postcss + tailwind setup

---
+++ src/lib/firebase.js
@@ -0,0 +1,19 @@
+import Firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+// seed file called
+import { seedDatabase } from "../seed";
+const config = {
+  apiKey: "AIzaSyD5Qbyi-PCfr58kTsa_gO6Zqwd2HnodjSc",
+  authDomain: "instagram-deni.firebaseapp.com",
+  projectId: "instagram-deni",
+  storageBucket: "instagram-deni.appspot.com",
+  messagingSenderId: "850585556330",
+  appId: "1:850585556330:web:7863163ed33be8d6a64113",
+};
+
+const firebase = Firebase.initializeApp(config);
+const { FieldValue } = Firebase.firestore;
+seedDatabase(firebase);
+export { firebase, FieldValue };
