f24b6e9abae9961f26bdc50bf9694b571265ad38
Stephan <stephangdenis@gmail.com>
2021-06-30 19:12:42 -0300

    [use-user] hook to get user uid with firebase

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -9,3 +9,18 @@ export async function doesUsernameExist(username) {
   //alows to check if the user exists in the firestore
   return result.docs.map((user) => user.data().length > 0);
 }
+
+export async function getUserByUserId(userId) {
+  const result = await firebase
+    .firestore()
+    .collection("users")
+    .where("userId", "==", userId)
+    .get();
+
+  const user = result.docs.map((item) => ({
+    ...item.data(), // we spread the users information
+    docId: item.id, // we also add the docId
+  }));
+
+  return user;
+}
