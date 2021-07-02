b00e2bb2625ad4dc6b0969b526ac51e875652660
Stephan <stephangdenis@gmail.com>
2021-07-02 00:37:58 -0300

    [Comments][AddComments]: comment functionality

--- src/components/post/Comments.js
+++ src/components/post/Comments.js
@@ -2,13 +2,13 @@ import { useState } from "react";
 import PropTypes from "prop-types";
 import { formatDistance } from "date-fns";
 import { Link } from "react-router-dom";
-
+import AddComment from "./AddComment";
 const Comments = ({ docId, comments: allComments, posted, commentInput }) => {
   const [comments, setComments] = useState(allComments);
   return (
     <>
       <div className="p-4 pt-1 pb-4">
-        {comments.length >= 3 && (
+        {comments.length >= 4 && (
           <p className="text-sm text-gray-base mb-1 cursor-pointer">
             View all {comments.length} comments
           </p>
@@ -25,6 +25,12 @@ const Comments = ({ docId, comments: allComments, posted, commentInput }) => {
           {formatDistance(posted, new Date())} ago
         </p>
       </div>
+      <AddComment
+        docId={docId}
+        comments={comments}
+        setComments={setComments}
+        commentInput={commentInput}
+      />
     </>
   );
 };
